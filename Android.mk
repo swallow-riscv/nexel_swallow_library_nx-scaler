cfa7d6ffe76115cde55b1cd18a3e8b16a41ce680
Hyejung Kwon <cjscld15@nexell.co.kr>
2017-10-13 12:09:32 +0900

    add makefile for Android
    this patch adds makefile for Android
    
    Change-Id: Id6ee9a893ba0cd7a78ddb37a08bdd42648400954
    Signed-off-by: Hyejung Kwon <cjscld15@nexell.co.kr>

--- Android.mk
+++ Android.mk
@@ -1,43 +1,18 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
-
-#
-#	Compile Flags
-#
-
-#
-#	Compile Options
-#
-
-LOCAL_CFLAGS += -DPLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION)
-
-LOCAL_C_INCLUDES := \
-	$(LOCAL_PATH)/include		\
-	system/core/include			\
-	system/media/core/include 	\
-	system/core/libsync/include \
-	frameworks/native/include 	\
-	frameworks/av/include 		\
-	hardware/nexell/s5pxx18/gralloc
-
-LOCAL_SHARED_LIBRARIES :=	\
-	liblog 		\
-	libcutils	\
-	libhardware
-
-LOCAL_SRC_FILES := \
-	nx-scaler.c
-
-# We need this because the current asm generates the following link error:
-# requires unsupported dynamic reloc R_ARM_REL32; recompile with -fPIC
-# Bug: 16853291
-LOCAL_LDFLAGS := -Wl,-Bsymbolic
-LOCAL_LDFLAGS += \
-	-L$(LINUX_LIBS)
 
 LOCAL_MODULE := libnx_scaler
 
-LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := \
+	nx-scaler.c
+LOCAL_SHARED_LIBRARIES := \
+	liblog \
+	libcutils \
+	libhardware
+LOCAL_C_INCLUDES += \
+	system/core/include/utils \
+	frameworks/native/include \
+	$(call include-path-for)
 
 include $(BUILD_SHARED_LIBRARY)
-
